e7073eff159e4d40e96144f3e75fd38061067f9d
sylwiales <slwlsnk@gmail.com>
2025-11-05 15:41:11 +0100

    działa

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -1,100 +1,37 @@
 <?php
 
 
-class SecurityController extends AppController
-{
+class AppController {
 
-    // ======= LOKALNA "BAZA" UŻYTKOWNIKÓW =======
-    private static array $users = [
-        [
-            'email' => 'anna@example.com',
-            'password' => '$2y$10$wz2g9JrHYcF8bLGBbDkEXuJQAnl4uO9RV6cWJKcf.6uAEkhFZpU0i', // test123
-            'first_name' => 'Anna'
-        ],
-        [
-            'email' => 'bartek@example.com',
-            'password' => '$2y$10$fK9rLobZK2C6rJq6B/9I6u6Udaez9CaRu7eC/0zT3pGq5piVDsElW', // haslo456
-            'first_name' => 'Bartek'
-        ],
-        [
-            'email' => 'celina@example.com',
-            'password' => '$2y$10$Cq1J6YMGzRKR6XzTb3fDF.6sC6CShm8kFgEv7jJdtyWkhC1GuazJa', // qwerty
-            'first_name' => 'Celina'
-        ],
-    ];
-
-    public function __construct()
+    protected function isGet(): bool
     {
-        parent::__construct();
+        return $_SERVER["REQUEST_METHOD"] === 'GET';
     }
 
-    public function login()
+    protected function isPost(): bool
     {
-        if (!$this->isPost()) {
-            return $this->render('login');
-        }
-
-        $email = $_POST["email"] ?? '';
-        $password = $_POST["password"] ?? '';
-
-        if (empty($email) || empty($password)) {
-            return $this->render('login', ['messages' => 'Fill all fields']);
-        }
-
-       //TODO replace with search from database
-        $userRow = null;
-        foreach (self::$users as $u) {
-            if (strcasecmp($u['email'], $email) === 0) {
-                $userRow = $u;
-                break;
-            }
-        }
-
-        if (!$userRow) {
-            return $this->render('login', ['messages' => 'User not found']);
-        }
-
-        if (!password_verify($password, $userRow['password'])) {
-            return $this->render('login', ['messages' => 'Wrong password']);
-        }
-
-        // TODO możemy przechowywać sesje użytkowika lub token
-        // setcookie("username", $userRow['email'], time() + 3600, '/');
-
-        $url = "http://$_SERVER[HTTP_HOST]";
-        header("Location: {$url}/dashboard");
+        return $_SERVER["REQUEST_METHOD"] === 'POST';
     }
 
-    public function register()
+    protected function render(string $template = null, array $variables = [])
     {
-        if (!$this->isPost()) {
-            return $this->render('register');
+        $templatePath = 'public/views/'. $template.'.html';
+        $templatePath404 = 'public/views/404.html';
+        $output = "";
+                 
+        if(file_exists($templatePath)){
+            // ['items' => $cards]
+            extract($variables);
+            // $items = [ [id=> 1], [id=>2]]
+            ob_start();
+            include $templatePath;
+            $output = ob_get_clean();
+        } else {
+            ob_start();
+            include $templatePath404;
+            $output = ob_get_clean();
         }
-
-        $email = trim($_POST['email'] ?? '');
-        $password = $_POST['password'] ?? '';
-        $firstName = $_POST['first-name'] ?? '';
-
-        if (empty($email) || empty($password) || empty($firstName)) {
-            return $this->render('register', ['messages' => 'Fill all fields']);
-        }
-
-	// TODO this will be checked in database
-        foreach (self::$users as $u) {
-            if (strcasecmp($u['email'], $email) === 0) {
-                return $this->render('register', ['messages' => 'Email is taken']);
-            }
-        }
-
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-        self::$users[] = [
-            'email' => $email,
-            'password' => $hashedPassword,
-            'first_name' => $firstName
-        ];
-
-        $url = "http://$_SERVER[HTTP_HOST]";
-        header("Location: {$url}/login");
+        echo $output;
     }
+
 }
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,43 +2,100 @@
 
 require_once 'AppController.php';
 
+class SecurityController extends AppController
+{
 
-class SecurityController extends AppController {
+    // ======= LOKALNA "BAZA" UŻYTKOWNIKÓW =======
+    private static array $users = [
+        [
+            'email' => 'anna@example.com',
+            'password' => '$2y$10$wz2g9JrHYcF8bLGBbDkEXuJQAnl4uO9RV6cWJKcf.6uAEkhFZpU0i', // test123
+            'first_name' => 'Anna'
+        ],
+        [
+            'email' => 'bartek@example.com',
+            'password' => '$2y$10$fK9rLobZK2C6rJq6B/9I6u6Udaez9CaRu7eC/0zT3pGq5piVDsElW', // haslo456
+            'first_name' => 'Bartek'
+        ],
+        [
+            'email' => 'celina@example.com',
+            'password' => '$2y$10$Cq1J6YMGzRKR6XzTb3fDF.6sC6CShm8kFgEv7jJdtyWkhC1GuazJa', // qwerty
+            'first_name' => 'Celina'
+        ],
+    ];
 
-    // TODO dekarator, który definiuje, jakie metody HTTP są dostępne
-    public function login() {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-        if($this->isGet()) {
-            return $this->render("login");
-        } 
+    public function login()
+    {
+        if (!$this->isPost()) {
+            return $this->render('login');
+        }
 
         $email = $_POST["email"] ?? '';
         $password = $_POST["password"] ?? '';
 
-        var_dump($email, $password);
-        // TODO get data from database
-
-         $this->render("dashboard");
-    }
-
-    public function register() {
-        // TODO pobranie z formularza email i hasła
-        // TODO insert do bazy danych
-        // TODO zwrocenie informajci o pomyslnym zarejstrowaniu
-
-        if ($this->isGet()) {
-            return $this->render("register");
+        if (empty($email) || empty($password)) {
+            return $this->render('login', ['messages' => 'Fill all fields']);
         }
 
-        $email = $_POST["email"] ?? '';
-        $password1 = $_POST["password1"] ?? '';
-        $password2 = $_POST["password2"] ?? '';
-        $firstname = $_POST["firstname"] ?? '';
-        $lastname = $_POST["lastname"] ?? '';
+       //TODO replace with search from database
+        $userRow = null;
+        foreach (self::$users as $u) {
+            if (strcasecmp($u['email'], $email) === 0) {
+                $userRow = $u;
+                break;
+            }
+        }
 
+        if (!$userRow) {
+            return $this->render('login', ['messages' => 'User not found']);
+        }
 
-        // TODO insert to database user
+        if (!password_verify($password, $userRow['password'])) {
+            return $this->render('login', ['messages' => 'Wrong password']);
+        }
 
-        return $this->render("login", ["message" => "Zarejestrowano uytkownika ".$email]);
+        // TODO możemy przechowywać sesje użytkowika lub token
+        // setcookie("username", $userRow['email'], time() + 3600, '/');
+
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/dashboard");
+    }
+
+    public function register()
+    {
+        if (!$this->isPost()) {
+            return $this->render('register');
+        }
+
+        $email = trim($_POST['email'] ?? '');
+        $password = $_POST['password'] ?? '';
+        $firstName = $_POST['first-name'] ?? '';
+
+        if (empty($email) || empty($password) || empty($firstName)) {
+            return $this->render('register', ['messages' => 'Fill all fields']);
+        }
+
+	// TODO this will be checked in database
+        foreach (self::$users as $u) {
+            if (strcasecmp($u['email'], $email) === 0) {
+                return $this->render('register', ['messages' => 'Email is taken']);
+            }
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+        self::$users[] = [
+            'email' => $email,
+            'password' => $hashedPassword,
+            'first_name' => $firstName
+        ];
+
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/login");
     }
 }
