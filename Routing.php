8631ec22d8340de2daa2716d5e81d9b7f3d97a35
sylwiales <slwlsnk@gmail.com>
2025-11-05 14:39:31 +0100

    karty

--- Routing.php
+++ Routing.php
@@ -1,14 +1,42 @@
 <?php
+
+require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/DashboardController.php';
+
 class Routing {
+
+    public static $routes = [
+        'login' => [
+            'controller' => "SecurityController",
+            'action' => 'login'
+        ],
+        'register' => [
+            'controller' => "SecurityController",
+            'action' => 'register'
+        ],
+        'dashboard' => [
+            'controller' => "DashboardController",
+            'action' => 'dashboard'
+        ]
+        ];
 
     public static function run(string $path) {
         switch ($path) {
-    case 'dashboard':
-        include 'public/views/dashboard.html';
-        break;
-    case 'login':
-        include 'public/views/login.html';
-        break;
+            case 'dashboard':
+                $controller_name = Routing::$routes[$path]["controller"];
+                $controller = new $controller_name;
+                
+                $action = Routing::$routes[$path]["action"];
+                $controller->$action();
+                break;
+                
+            case 'login':
+                $controller_name = Routing::$routes[$path]["controller"]; 
+                $controller = new $controller_name; 
+
+                $action = Routing::$routes[$path]["action"];
+                $controller->$action(); 
+                break;
     default:
         include 'public/views/404.html';
         break;
